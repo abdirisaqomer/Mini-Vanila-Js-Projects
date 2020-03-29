f4085290740962ba83a3add9dc26b3717a94e06d
unknown <abdirisaq.omer@nology.io>
2020-03-29 23:22:53 +0100

    More Js Work

--- Form-Validation/main.js
+++ Form-Validation/main.js
@@ -13,21 +13,29 @@ const showError = (input, message) => {
 // Show input success message
 const showSuccess = (input, message) => {
     const formControl = input.parentElement;
-    formControl.className = "form-control succeess";
+    formControl.className = "form-control success";
     const small = formControl.querySelector('small');
     small.innerText = message;
+}
+
+// Check email is valid
+
+const isValidEmail(email) {
+    const re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    return re.test(String(email).toLowerCase());
 }
 
 //Event Listeners
 form.addEventListener('submit', (event) => {
     event.preventDefault();
 
-    if (username.value === '') {
-        showError(username, 'username is needed');
-    } else {
-        showSuccess(username);
-    }
-    // let enteredValue = username.value === '' ? showError('Username is required') : showSuccess(username);
+    let enteredUserName = username.value === '' ? showError(username, 'Username is required') : showSuccess(username);
+
+    let enteredEmail = email.value === '' ? showError(email, 'Email is required') : showSuccess(email);
+
+    let enteredPassword  = password.value === '' ? showError(password, 'Password is required') : showSuccess(password);
+
+    let enteredPassword2 = password2.value === '' ? showError(password2, 'Password 2 is required') : showSuccess(password2);
 });
 
 
