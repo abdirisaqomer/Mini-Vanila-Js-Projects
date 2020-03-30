b3c93a740a586572c2ffb545efa286c473d20ed6
unknown <abdirisaq.omer@nology.io>
2020-03-30 01:15:27 +0100

    More JS work

--- Form-Validation/main.js
+++ Form-Validation/main.js
@@ -8,34 +8,33 @@ const password2 = document.getElementById('password2');
 const showError = (input, message) => {
     const formControl = input.parentElement;
     formControl.className = "form-control error";
-}
-
-// Show input success message
-const showSuccess = (input, message) => {
-    const formControl = input.parentElement;
-    formControl.className = "form-control success";
     const small = formControl.querySelector('small');
     small.innerText = message;
 }
 
-// Check email is valid
+// Show input success outline
+const showSuccess = (input) => {
+    const formControl = input.parentElement;
+    formControl.className = "form-control success";
+}
 
-const isValidEmail(email) {
+// Check email is valid
+const isValidEmail = (email) => {
     const re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
     return re.test(String(email).toLowerCase());
 }
 
 //Event Listeners
-form.addEventListener('submit', (event) => {
-    event.preventDefault();
+form.addEventListener('submit', (e) => {
+    e.preventDefault();
 
-    let enteredUserName = username.value === '' ? showError(username, 'Username is required') : showSuccess(username);
+    const enteredUserName = username.value === '' ? showError(username, 'Username is required') : showSuccess(username);
 
-    let enteredEmail = email.value === '' ? showError(email, 'Email is required') : showSuccess(email);
+    const enteredEmail = email.value === '' ? showError(email, 'Email is required') : !isValidEmail(email.value) ? showError(email, 'Email is not valid') : showSuccess(email);
 
-    let enteredPassword  = password.value === '' ? showError(password, 'Password is required') : showSuccess(password);
+    const enteredPassword  = password.value === '' ? showError(password, 'Password is required') : showSuccess(password);
 
-    let enteredPassword2 = password2.value === '' ? showError(password2, 'Password 2 is required') : showSuccess(password2);
+    const enteredPassword2 = password2.value === '' ? showError(password2, 'Password 2 is required') : showSuccess(password2);
 });
 
 
